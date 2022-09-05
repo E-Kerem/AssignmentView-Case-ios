bd82fbcb9f6fafa907fa23a62b19dd45f2abfd8d
Efe Kerem Kesgin <kesginkerem@gmail.com>
2022-09-05 16:28:12 +0300

    Initial Commit

---
+++ AssignmentView-Case-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentView-Case-ios
+//
+//  Created by Efe Kerem Kesgin on 5.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
